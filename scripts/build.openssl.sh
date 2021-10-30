aead7ca32c466e1885a4569056613a61b23d08d7
hintro32 <81028826+hintro32@users.noreply.github.com>
2021-10-30 09:39:57 -0400

    Update build.openssl.sh

--- scripts/build.openssl.sh
+++ scripts/build.openssl.sh
@@ -13,7 +13,7 @@ tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
 
 cd openssl-${OPENSSL_VERSION}
 ./config -no-shared -no-asm -no-zlib -no-comp -no-dgram -no-filenames -no-cms
-make -j$((`nproc --all`*2)+2)
+make -j$((`nproc --all`*3))
 cp -fr include ../../deps
 cp libcrypto.a ../../deps/lib
 cp libssl.a ../../deps/lib
