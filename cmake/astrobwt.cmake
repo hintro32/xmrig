5c4413478b28ad4fb7df0172d3126a08a01e97ad
hintro32 <81028826+hintro32@users.noreply.github.com>
2021-11-02 16:09:20 -0400

    Update astrobwt.cmake

--- cmake/astrobwt.cmake
+++ cmake/astrobwt.cmake
@@ -26,8 +26,7 @@ if (WITH_ASTROBWT)
             list(APPEND SOURCES_CRYPTO src/crypto/astrobwt/xmm6int/salsa20_xmm6int-avx2.c)
 
             if (CMAKE_C_COMPILER_ID MATCHES GNU OR CMAKE_C_COMPILER_ID MATCHES Clang)
-                set_source_files_properties("src/crypto/astrobwt/xmm6int/salsa20_xmm6int-avx2.c
-                src/crypto/astrobwt/salsa20_ref/salsa20.c src/crypto/astrobwt/Salsa20.cpp " PROPERTIES COMPILE_OPTIONS -march=znver2)
+                set_source_files_properties(src/crypto/astrobwt/xmm6int/salsa20_xmm6int-avx2.c PROPERTIES COMPILE_OPTIONS -mavx2)
             endif()
 
             if (CMAKE_C_COMPILER_ID MATCHES MSVC)
